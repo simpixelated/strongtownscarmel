4a11ef1387e220173149104b50c9dda1ee8a7b4d
Jordan Kohl <jordan.kohl@segment.com>
2025-05-15 10:13:02 -0400

    adds homepage about us section

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+import fontAwesomePlugin from "@11ty/font-awesome";
 
 import pluginFilters from "./_config/filters.js";
 
@@ -101,6 +102,8 @@ export default async function(eleventyConfig) {
 		// slugify: eleventyConfig.getFilter("slugify"),
 		// selector: "h1,h2,h3,h4,h5,h6", // default
 	});
+
+	eleventyConfig.addPlugin(fontAwesomePlugin);
 
 	eleventyConfig.addShortcode("currentBuildDate", () => {
 		return (new Date()).toISOString();
